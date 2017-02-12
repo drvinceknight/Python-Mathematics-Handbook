cc972b8eb8e523b8d05342843daab8726136a0c5
Vince Knight <vincent.knight@gmail.com>
2017-02-12 15:12:09 +0000

    Add script to create pdfs.

---
+++ tools/make.py
@@ -0,0 +1,13 @@
+import glob
+import subprocess
+import os
+
+os.chdir("nbs")
+ipynbs = glob.glob("*ipynb")
+
+for ipynb in ipynbs:
+    tex = ipynb[:-6] + ".tex"
+    pdf = ipynb[:-6] + ".pdf"
+    subprocess.call(["jupyter-nbconvert", "--to", "latex", ipynb])
+    subprocess.call(["latexmk", "--xelatex", tex])
+    subprocess.call(["latexmk", "-c"])
